103e2ca87d16e719296d8d910f4edb82aa231113
exxxpm <127376127+exxxpm@users.noreply.github.com>
2025-11-27 22:00:35 +0400

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def index():
       </head>
       <body class="d-flex align-items-center justify-content-center vh-100">
         <div class="card p-4" style="width: 650px;">
-          <h3 class="text-center mb-3">🚀 DevOPS Lab App</h3>
+          <h3 class="text-center mb-3">🚀 DevOPS Lab Apppp</h3>
           <div class="text-center mb-4">
             <span class="badge status-badge">✅ DEPLOYED SUCCESSFULLY</span>
           </div>
